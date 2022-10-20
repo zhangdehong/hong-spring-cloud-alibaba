cc955fa18ec7a4e847820a493a9d1a6206cb955a
晴天 <9324209+z-de-hong@user.noreply.gitee.com>
2022-10-20 13:56:03 +0800

    增加服务注册nacos

--- hong-provider/src/main/java/com/hong/provider/ProviderApplication.java
+++ hong-provider/src/main/java/com/hong/provider/ProviderApplication.java
@@ -2,6 +2,7 @@ package com.hong.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author: ZhangDeHong
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @Date Create by  22:07 2022/7/17
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProviderApplication {
 
     public static void main (String[] args) {
